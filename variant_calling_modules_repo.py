4da1b316fe25929eca14d8b1066a762950dcfc89
Akanksha Khorgade <35128986+akankshakhorgade@users.noreply.github.com>
2019-12-04 14:05:29 -0500

    variant_calling_modules_repo.py
    Repository for all the modules to be called within the subprocesses and the main script for the variant calling workflow

---
+++ variant_calling_modules_repo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import re
+import os
+import sys
+import json
+import argparse
+import numpy as np
+import pandas as pd
+import datetime as dt
+import openpyxl as excel
+#from google.cloud import storage
+
+#example module: to align
+def bwaAlign():
+    return()
+
+# to compute GC Bias Metrics:
+def collectGCBiasMetrics():
+    return()
+
+def collectInsertSizeMetrics():
+    return()
+
+
