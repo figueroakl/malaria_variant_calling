8a6e0bfec974f176bf930f74fe6605ab7bb9c932
RUCHIT PANCHAL <rpanchal@broadinstitute.org>
2019-12-13 16:27:52 -0500

    Added content for QC steps on bams. Still need to add steps for processing/aggregating output and append to summary file.

--- QC_subProc.py
+++ QC_subProc.py
@@ -3,18 +3,26 @@ import argparse
 
 from variant_calling_modules_repo \
     import collectGCBiasMetrics, \
-    collectInsertSizeMetrics
+    collectInsertSizeMetrics, \
+    qcheck
 
 
 def main():
     # init cmd-line parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('--metafile_path', required=True, help="Path to metafile excel")
+    parser.add_argument('--metafile_path', required=True, help="Path to metafile excel or any other tsv")
+    parser.add_argument('--reference', required=True, help="Path to the reference genome fasta file")
+    parser.add_argument('--path_to_picard', required=True, help="Path to picard cmd line tools")
     parser.add_argument('--upload_to_GC', default='gs://bucket/', type=str,
                         help="Directory to upload ouput on GC.")
     parser.add_argument('--verbose', action="store_true") # verbose replace verbose in if with dry run
     parser.add_argument('--dry_run', action="store_true")
     args = parser.parse_args()
 
-
+    metafile = args.metafile_path
+    ref = args.reference
+    picard = args.path_to_picard
+    
+    qcheck(metafile, ref, picard)
+    
 
--- variant_calling_modules_repo.py
+++ variant_calling_modules_repo.py
@@ -15,11 +15,35 @@ import openpyxl as excel
 def bwaAlign():
     return()
 
+# QC metrics wrapper
+def qcheck(metaPath, refPath, path_to_picard):
+	if os.path.isfile(metaPath):
+        metadf = pd.read_csv(metaPath, sep = '\t')
+        if metadf.empty:
+            raise Exception('Input Meta File Found empty')
+        else:
+        	for num in range(len(metadf)):
+        		sampleid = metadf.iloc[num,0]
+				ipath = metadf.iloc[num,1]
+                collectGCBiasMetrics(path_to_picard, sampleid, ipath, refPath)
+                collectInsertSizeMetrics(path_to_picard, sampleid, ipath, refPath)
+				
+
+    else:
+    	raise Exception('Input Metafile not found: The path mentioned is'.format(metaPath))
+	return()
+
 # to compute GC Bias Metrics
-def collectGCBiasMetrics():
+def collectGCBiasMetrics(path_to_picard, sampleid, ipath, refPath):
+    opath = sampleid + "_gc_bias_metrics.txt"
+    cmd = path_to_picard + "CollectGcBiasMetrics I=" + ipath + " O=" + opath + " R=" + refPath
+    os.system(cmd)
     return()
 
 # to compute Insert Size Metrics
-def collectInsertSizeMetrics():
+def collectInsertSizeMetrics(path_to_picard, sampleid, ipath):
+    opath = sampleid + "insert_size_metrics.txt"
+    cmd = path_to_picard + "CollectInsertSizeMetrics I=" + ipath + " O=" + opath
+    os.system(cmd)
     return()
 
