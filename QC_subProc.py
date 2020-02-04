95ea1746090b12c8e5d0f646945f43174276a075
RUCHIT PANCHAL <rpanchal@broadinstitute.org>
2020-02-04 12:16:22 -0500

    updates to gc metrics file processing

--- QC_subProc.py
+++ QC_subProc.py
@@ -13,8 +13,8 @@ def main():
     parser.add_argument('--metafile_path', required=True, help="Path to metafile excel or any other tsv")
     parser.add_argument('--reference', required=True, help="Path to the reference genome fasta file")
     parser.add_argument('--path_to_picard', required=True, help="Path to picard cmd line tools")
-    parser.add_argument('--upload_to_GC', default='gs://bucket/', type=str,
-                        help="Directory to upload ouput on GC.")
+    #parser.add_argument('--upload_to_GC', default='gs://bucket/', type=str,
+    #                    help="Directory to upload ouput on GC.")
     parser.add_argument('--verbose', action="store_true") # verbose replace verbose in if with dry run
     parser.add_argument('--dry_run', action="store_true")
     args = parser.parse_args()
--- variant_calling_modules_repo.py
+++ variant_calling_modules_repo.py
@@ -22,21 +22,35 @@ def qcheck(metaPath, refPath, path_to_picard):
         if metadf.empty:
             raise Exception('Input Meta File Found empty')
         else:
+            fqc = open("qc_summary.tsv", "a+")
         	for num in range(len(metadf)):
         		sampleid = metadf.iloc[num,0]
 				ipath = metadf.iloc[num,1]
                 gc_met = collectGCBiasMetrics(path_to_picard, sampleid, ipath, refPath)
                 ins_met = collectInsertSizeMetrics(path_to_picard, sampleid, ipath, refPath)
-				
+				if os.path.isfile(gc_met):
+                    gc_df = pd.read_csv(gc_met, sep = '\t', skiprows = 6)
+                    maxgc = gc_df['NORMALIZED_COVERAGE'].idxmax()
+                    qc_summary = pd.DataFrame(data = {'sampleid' : [sampleid], 'MaxCoverage_GC' : [maxgc]})
+                    fqc.write(qc_summary)
+                else:
+                    raise Exception('Path to GcMetrics output File does not exist: Path given'.format(gc_met))
+                if os.path.isfile(ins_met):
+                    ins_df = pd.read_csv(ins_met, sep = '\t')
+                else:
+                    raise Exception('Path to InsertsizeMetrics File does not exist: Path given'.format(ins_met))
 
     else:
     	raise Exception('Input Metafile not found: The path mentioned is'.format(metaPath))
-	return()
+	fqc.close()
+    return()
 
 # to compute GC Bias Metrics
 def collectGCBiasMetrics(path_to_picard, sampleid, ipath, refPath):
     opath = sampleid + "_gc_bias_metrics.txt"
-    cmd = path_to_picard + "CollectGcBiasMetrics I=" + ipath + " O=" + opath + " R=" + refPath
+    chart = " CHART_OUTPUT=" + sampleid + "_chart_output.pdf"
+    summary = " s=" sampleid + "_summary_output.txt"
+    cmd = path_to_picard + "CollectGcBiasMetrics I=" + ipath + " O=" + opath + " R=" + refPath + chart + summary
     err = os.system(cmd)
     if err:
         print 'Execution of "%s" failed!\n' % cmd
@@ -52,4 +66,3 @@ def collectInsertSizeMetrics(path_to_picard, sampleid, ipath):
         print 'Execution of "%s" failed!\n' % cmd
         sys.exit(1)
     return(opath)
-
